6389e49f9c79cdd5b3a3b4f1815b934dc0c0a881
Podruzsik László <98643714+laszlopodruzsik@users.noreply.github.com>
2023-04-06 11:55:28 +0200

    Vizsga

---
+++ ts module/TypeScript.ts
@@ -0,0 +1,11 @@
+function PhErtek(i:number):string{
+    if(i==7)return'semleges'
+    else if(i>7)return 'lugos'
+    else return 'savas'
+}
+
+const random = [1,3,5,6,7,11,25,21];
+function PrimekSzama(p):number{
+    for(let i;i<p;i++){p}
+    return()
+}
